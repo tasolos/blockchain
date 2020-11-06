ecbdbb9abad20e01e4b0f59f6e709d5a6c6b8260
Tasos <tasosgarinis@gmail.com>
2020-11-06 13:52:19 +0200

    msg

--- src/Register.js
+++ src/Register.js
@@ -1,48 +1,48 @@
-import React from 'react';
-import styled from 'styled-components';
-import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBInput } from 'mdbreact';
+import React from "react";
+import './Register.css';
 
-const GridWrapper = styled.div`
-  display: grid;
-  grid-gap: 10px;
-  margin-top: 1em;
-  margin-left: 6em;
-  margin-right: 6em;
-  grid-template-columns: repeat(12, 1fr);
-  grid-auto-rows: minmax(25px, auto);
-`;
 
-const FormPage = () => {
-return (
-<MDBContainer>
-  <MDBRow>
-    <MDBCol md="6">
-      <form>
-        <p className="h5 text-center mb-4">Sign up</p>
-        <div className="grey-text">
-          <MDBInput label="Your name" icon="user" group type="text" validate error="wrong"
-            success="right" />
-          <MDBInput label="Your email" icon="envelope" group type="email" validate error="wrong"
-            success="right" />
-          <MDBInput label="Confirm your email" icon="exclamation-triangle" group type="text" validate
-            error="wrong" success="right" />
-          <MDBInput label="Your password" icon="lock" group type="password" validate />
-        </div>
-        <div className="text-center">
-          <MDBBtn color="primary">Register</MDBBtn>
-        </div>
-      </form>
-    </MDBCol>
-  </MDBRow>
-</MDBContainer>
-);
-};
 
-export default FormPage;
+export const Register = () => {
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  
 
-export const Register = (props) => (
-  <GridWrapper>
-    <p>This is a paragraph and I am writing on the home page</p>
-    <p>This is another paragraph, hi hey hello whatsup yo</p>
-  </GridWrapper>
-)
+  const handleSubmit = (event) => {
+    console.log(`
+      Email: ${email}
+      Password: ${password}
+    `);
+
+    event.preventDefault();
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h1>Create Account</h1>
+
+      <label>
+        Email:
+        <input
+          name="email"
+          type="email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          required />
+      </label>
+
+      <label>
+        Password:
+        <input
+          name="password"
+          type="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          required />
+      </label>
+
+      <button id="Submit">Submit</button >
+    </form>
+  );
+}
+
